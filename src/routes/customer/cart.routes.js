d34b202bfe0a649ed929673584f0e696d5096b4d
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-06-10 10:00:42 +0200

    Created the product routes for customers

---
+++ src/routes/customer/cart.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { 
+    addToCart,
+    getCartItems,
+    updateCartItem,
+    removeFromCart 
+} = require('../../controllers/customer/cart.controller');
+const { authMiddleware } = require('../../middleware/auth');
+
+router.use(authMiddleware);
+router.post('/', addToCart);
+router.get('/', getCartItems);
+router.put('/:id', updateCartItem);
+router.delete('/:id', removeFromCart);
+
+module.exports = router;
