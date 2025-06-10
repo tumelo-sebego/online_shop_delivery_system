70d155f73b6f81248c3b63d031a647f9119cfe58
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-06-10 10:02:09 +0200

    Created cart routes

--- src/routes/customer/cart.routes.js
+++ src/routes/customer/cart.routes.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { 
-    addToCart,
-    getCartItems,
-    updateCartItem,
-    removeFromCart 
-} = require('../../controllers/customer/cart.controller');
-const { authMiddleware } = require('../../middleware/auth');
+const {
+  addToCart,
+  getCartItems,
+  updateCartItem,
+  removeFromCart,
+} = require("../../controllers/customer/cart.controller");
+const { authMiddleware } = require("../../middleware/auth");
 
 router.use(authMiddleware);
-router.post('/', addToCart);
-router.get('/', getCartItems);
-router.put('/:id', updateCartItem);
-router.delete('/:id', removeFromCart);
+router.post("/", addToCart);
+router.get("/", getCartItems);
+router.put("/:id", updateCartItem);
+router.delete("/:id", removeFromCart);
 
 module.exports = router;
