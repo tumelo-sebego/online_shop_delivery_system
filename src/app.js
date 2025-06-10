d889e2c47f66ab080920f7588e7c88e36d82e907
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-06-10 09:35:29 +0200

    updated the main app.js

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/common/auth.routes');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
