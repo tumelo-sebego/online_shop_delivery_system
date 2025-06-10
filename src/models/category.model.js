5e8d953ea4741897bfdbfa2d95d8a4ae7fc74417
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-06-10 14:18:32 +0200

    created the Category db model

---
+++ src/models/category.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
+);
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
