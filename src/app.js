788ea981057119736fe74c5469536f25eddef650
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-06-10 14:34:51 +0200

    updated the main app file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const connectDB = require("./config/database");
 const authRoutes = require("./routes/common/auth.routes");
 const adminRoutes = require("./routes/admin");
@@ -7,6 +9,13 @@ const customerRoutes = require("./routes/customer");
 const driverRoutes = require("./routes/driver");
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*", // Configure according to your needs
+    methods: ["GET", "POST"],
+  },
+});
 
 // Connect to MongoDB
 connectDB();
@@ -14,6 +23,25 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// WebSocket connection handling
+io.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("join-order", (orderId) => {
+    socket.join(`order-${orderId}`);
+    console.log(`Client joined order: ${orderId}`);
+  });
+
+  socket.on("driver-location", (data) => {
+    socket.to(`order-${data.orderId}`).emit("location-update", data);
+    console.log(`Location update for order: ${data.orderId}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
 
 // Root route
 app.get("/", (req, res) => {
@@ -37,8 +65,8 @@ app.use("/api/driver", driverRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
