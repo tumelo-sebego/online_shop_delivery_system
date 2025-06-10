092cbc3a28b7a5a99e8630ffac4b23adafb7d6fa
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-06-10 14:42:12 +0200

    created the authentication middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,16 +1,30 @@
+const { verifyToken } = require("../utils/jwt");
+
 const authMiddleware = (req, res, next) => {
-    try {
-        const token = req.headers.authorization?.split(' ')[1];
-        if (!token) {
-            return res.status(401).json({ error: 'Authorization token required' });
-        }
-        // TODO: Implement token verification
-        next();
-    } catch (error) {
-        res.status(401).json({ error: 'Request is not authorized' });
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ error: "No token provided" });
     }
+
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+};
+
+const requireRole = (roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ error: "Access denied" });
+    }
+    next();
+  };
 };
 
 module.exports = {
-    authMiddleware
+  authMiddleware,
+  requireRole,
 };
