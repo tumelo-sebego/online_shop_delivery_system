d64abb7a0f8556e52bd5ae7f0d4e2938ee999367
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-06-10 13:49:04 +0200

    created User model

---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password_hash: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "customer", "driver"],
+      required: true,
+    },
+    first_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
